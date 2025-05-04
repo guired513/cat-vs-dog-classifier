cf801cceb03d0077d69ae57347e10708e93983c4
guired13 <red2006135@gmail.com>
2025-05-04 13:37:23 +0800

    Initial commit: project setup and dataset download

---
+++ explore_dataset.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import os
+from PIL import Image
+
+cat_dir = "data/training_set/training_set/cats"
+dog_dir = "data/training_set/training_set/dogs"
+
+cat_sample = os.path.join(cat_dir, os.listdir(cat_dir)[0])
+dog_sample = os.path.join(dog_dir, os.listdir(dog_dir)[0])
+
+fig, axs = plt.subplots(1, 2, figsize=(8, 4))
+axs[0].imshow(Image.open(cat_sample))
+axs[0].set_title("Sample Cat")
+axs[1].imshow(Image.open(dog_sample))
+axs[1].set_title("Sample Dog")
+plt.show()
